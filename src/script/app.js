dc82a0b1239c3a5816f4d8bb3decda44700b2a85
TheysKlaas <33479434+TheysKlaas@users.noreply.github.com>
2019-02-20 17:17:23 +0100

    ClearList issue

--- src/script/app.js
+++ src/script/app.js
@@ -1,8 +1,29 @@
 let countryHolder,
-    amountHolder;
+    amountHolder,
+    notificationHolder,
+    hamburgerMenu,
+    mainGrid,
+    sideBar;
+
 const localKey = 'travel-planner';
 
+const clearList = () => {
+    const button = document.querySelector('.js-reset-counter');
+    const countries = document.querySelectorAll('.js-country-checkbox');
 
+    button.addEventListener('click', function(){
+        localStorage.removeItem(localKey);
+        for (country in countries){
+            if (country.checked == true){
+                country.checked = false;
+            }
+            else{
+                country.checked = true;
+            }
+        }
+        updateCounter();
+    })
+}
 
 const addItem = key => {
     let countries = getAllItems(); // array ophalen
@@ -35,11 +56,34 @@ const hasItem = key => {
 const updateCounter = () => {
    amountHolder.innerHTML = countItems();
 };
+const fadeAndRemoveNotification = (element) => {
+    element.classList.add('u-fade-out');
+    setTimeout(() => {
+        document.querySelector('.js-notification-holder').removeChild(element);
+    }, 800);
+}
+
+const showNotification = (element) => {
+    // 1 Show in js-notification-holder
+    let notification = document.createElement('div');
+    notification.classList.add('c-notification')
+    notification.innerHTML = `                
+        <h2 class="c-notification__header">You have selected ${element.dataset.countryName}</h2>
+        <button class="c-notification__action">UNDO</button>`
+
+    notificationHolder.append(notification);
+    // 2 fade out after 800ms
+    setTimeout(() => {
+        fadeAndRemoveNotification(notification);
+    }, 1500);
+    
+}
 
 const addListenersToCountries = function(classSelector){
     const countries = document.querySelectorAll(classSelector);
     for (const country of countries){
         country.addEventListener('click', function(){
+            console.log({country});
             console.log('you clicked me', this);
             // functies schrijven voor local storage => opsplitsen!!
             const countryKey = this.getAttribute('for');
@@ -48,6 +92,7 @@ const addListenersToCountries = function(classSelector){
             }
             else{
                 addItem(countryKey);
+                showNotification(country);
             }
             updateCounter();            
         })
@@ -88,8 +133,8 @@ const showCountries = data => {
         //2 build an html string for each country
         countries += `
         <article>
-                    <input id="${c.cioc}-${c.alpha2Code}" class="o-hide c-country-input" type="checkbox" name="" ${(hasItem(c.cioc + '-' + c.alpha2Code)) ? 'checked="checked"' : ''}>
-                    <label for="${c.cioc}-${c.alpha2Code}" class="c-country js-country">
+                    <input id="${c.cioc}-${c.alpha2Code}" class="o-hide c-country-input js-country-checkbox" type="checkbox" name="" ${(hasItem(c.cioc + '-' + c.alpha2Code)) ? 'checked="checked"' : ''}>
+                    <label for="${c.cioc}-${c.alpha2Code}" class="c-country js-country" data-country-name=${c.name}>
                         <div class="c-country-header">
                             <h2 class="c-country-header__name">${c.name}</h2>
                             <img class="c-country-header__flag" src="${c.flag}" alt="The flag of ${c.name}">
@@ -103,6 +148,7 @@ const showCountries = data => {
     countryHolder.innerHTML = countries;
     // Hoerah, html is loaded
     addListenersToCountries('.js-country')
+    clearList();
 
     //checkboxListener();
 
@@ -132,7 +178,16 @@ const enableListeners= () => {
     }
     countryHolder = document.querySelector('.js-country-holder');
     amountHolder =  document.querySelector('.js-amount');
+    notificationHolder = document.querySelector('.js-notification-holder')
+    hamburgerMenu = document.querySelector('.js-burger');
+    mainGrid = document.querySelector('.js-maingrid');
+    sideBar = document.querySelector('.js-sidebar');
 
+    hamburgerMenu.addEventListener('click', function(){
+        console.log('clicked')
+        mainGrid.classList.toggle('u-grid-active');
+        sideBar.classList.toggle('u-sidebar-active');
+    })
     // always start with Europe
     fetchCountries('europe');
     updateCounter();
